a937843b0adfbb77b798efa8be41786d31d23b96
Johnnathon Williams <jwill52492@gmail.com>
2024-12-27 21:06:15 -0500

    Add files via upload

---
+++ components/FormValidator.js
@@ -0,0 +1,79 @@
+class FormValidator {
+  constructor(settings, formEl) {
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorclass = settings.errorclass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formSelector = settings.formSelector;
+    this._formEl = formEl;
+    this.settings = settings;
+
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(
+        this._formEl,
+        inputElement,
+        inputElement.validationMessage,
+      );
+    } else {
+      hideInputError(inputElement);
+    }
+  }
+
+
+  _disableSubmitButton() {
+    this._submitButton = this.formEl.querySelector(this._submitButtonSelector);
+    this._submitButton.classList.add(this._inactiveButtonClass);
+    this._submitButton.disabled = true;
+  }
+
+  _enableSubmitButton() {
+    this._submitButton = this._formEl.querySelector(this._submitButtonSelector);
+    this._submitButton.classList.remove(this._inactiveButtonClass);
+    this._submitButton.disabled = false;
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._disableSubmitButton();
+    } else {
+      this._enableSubmitButton();
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    })
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector),
+    );
+    const buttonElement = this._formEl.querySelector(
+      this.settings.submitButtonSelector,
+    );
+
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+}
+
+export default FormValidator;
